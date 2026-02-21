124e1f7e51d19870ae1e942748cf7a8734dc0c37
Jon West <electrikjesus@gmail.com>
2026-02-21 10:08:37 -0500

    fix: updates to resolve compile issues

--- app/src/main/java/com/droidlogic/setupwizard/fragment/NetworkFragment.java
+++ app/src/main/java/com/droidlogic/setupwizard/fragment/NetworkFragment.java
@@ -25,6 +25,7 @@ import android.view.inputmethod.InputMethodManager;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
+import androidx.leanback.app.GuidedStepSupportFragment;
 import androidx.leanback.widget.GuidanceStylist;
 import androidx.leanback.widget.GuidedAction;
 import androidx.leanback.widget.GuidedActionAdapter;
